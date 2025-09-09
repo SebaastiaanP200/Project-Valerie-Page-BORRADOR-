6dfb417d2fe518123e270fb69878cda9fb688908
Sebastián Pérez <sebastiaan.peerez@gmail.com>
2025-09-09 20:20:36 -0300

    a

---
+++ js/portfolio.js
@@ -0,0 +1,20 @@
+/* LOGICA PORTFOLIO */
+// const carouselContainer = document.querySelector(".carousel__container");
+
+// const getImg = async () => {
+//     try {
+//     const res = await axios.get("img.json");
+//     const data = res.data;
+//     const containerImgHTML = document.createDocumentFragment();
+//     data.imagen.forEach(image => {
+//         const img = document.createElement("IMG");
+//         img.alt = image.titulo;
+//         img.src = image.url;
+//         img.classList.add("image")
+//         containerImgHTML.appendChild(img);
+//     });
+//     carrouselContainer.appendChild(containerImgHTML);
+// }catch(e) {console.error("Error durante la carga: ");}
+// }
+
+// getImg();
